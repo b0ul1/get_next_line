3dc865e1af200b3a446c5092d16f5955886b4b6e
b0ul1 <fbiarelooser@gmail.com>
2025-11-18 14:45:55 +0100

    get_next_line_utils_bonus.c

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trvukano <trvukano@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 18:21:20 by trvukano          #+#    #+#             */
+/*   Updated: 2023/10/30 18:47:54 by trvukano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (!c)
+		return ((char *)s + ft_strlen(s));
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*dest;
+	size_t	i;
+	size_t	j;
+
+	i = -1;
+	j = 0;
+	if (!s1)
+	{
+		s1 = (char *)malloc(sizeof(char) * 1);
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	dest = malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
+	if (!dest)
+		return (NULL);
+	if (s1)
+		while (s1[++i])
+			dest[i] = s1[i];
+	while (s2[j])
+		dest[i++] = s2[j++];
+	dest[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (dest);
+}
