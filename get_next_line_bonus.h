70c1cbb3d5351e2abb899f5d05698ee999062bde
b0ul1 <fbiarelooser@gmail.com>
2025-11-18 14:44:46 +0100

    get_next_line_bonus.h

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trvukano <trvukano@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 18:20:46 by trvukano          #+#    #+#             */
+/*   Updated: 2023/11/02 14:21:11 by trvukano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <limits.h>
+
+char	*ft_strjoin(char *line, char *buff);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*line_reader(int fd, char *line);
+char	*line_extractor(char *line);
+char	*line_remover(char *line);
+char	*get_next_line(int fd);
+
+#endif
